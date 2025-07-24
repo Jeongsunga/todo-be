1443e3af713d272c3ef8c5174f80c495a6ddea2f
Seunga Jeong <jsa3341@gmail.com>
2025-07-24 02:31:08 +0200

    make login part

---
+++ controllers/task.controller.js
@@ -0,0 +1,53 @@
+const Task = require("../models/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save();
+    res.status(200).json({ status: "success", data: newTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.getTask = async (req, res) => {
+  try {
+    const taskList = await Task.find({}).select("-__v");
+    res.status(200).json({ status: "success", data: taskList });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id, // URL로 받은 id 값 (예: /api/tasks/123)
+      req.body, // 요청 본문(body)에 담긴 업데이트할 데이터
+      {
+        new: true, // 수정된 Task 반환하도록 설정(기본값인 false는 수정 전 Task 반환)
+        runValidators: true, // Mongoose 스키마 유효성 검사 적용
+      }
+    );
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ status: "success", data: updatedTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const deleteItem = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ status: "success", data: deleteItem });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+module.exports = taskController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const taskAPI = require('./task.api')
+const taskApi = require('./task.api');
+const userApi = require('./user.api');
 
-router.use('/tasks', taskAPI);
+router.use('/tasks', taskApi);
+router.use('/user', userApi);
 
 module.exports = router;
